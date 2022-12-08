cc2a846560976ef35a2b7401932509596575cb06
heyzzin <tohj@realcoding.co>
2022-12-08 11:34:50 +0900

    tie align 수정

--- twp-home-public/js/app.js
+++ twp-home-public/js/app.js
@@ -13,11 +13,11 @@ readConfig((error, data) => {
 
         BX.components.Header.bx(homepage.header).appendTo(topBox);
         BX.components.SlideBanner.bx(homepage.banner).appendTo(topBox);
-        const tie = box().appendTo(topBox).align('center').maxWidth(1200).left('50%').css('transform', 'translate(-50%, 0)');
+        const tie = box().appendTo(topBox).maxWidth(1200).left('50%').css('transform', 'translate(-50%, 0)');
         BX.components.About.bx(homepage.about).appendTo(tie);
         BX.components.IconCard.bx(homepage.application).appendTo(tie);
         BX.components.NewsCard.bx(homepage.newsContent).appendTo(tie);
-        
+
         BX.components.Connect.bx(homepage.connect).appendTo(topBox);
         BX.components.Footer.bx().appendTo(topBox);
     }
